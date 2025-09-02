896f894a425631563711af0f20ff231f40edd572
RuannKarllos <ruannkarlloss@gmail.com>
2025-09-02 20:16:59 -0300

    update

--- src/main/java/br/com/mycompany/entity/User.java
+++ src/main/java/br/com/mycompany/entity/User.java
@@ -9,18 +9,43 @@ package br.com.mycompany.entity;
  * @author Ruann
  */
 public class User {
-    
+
     private String name;
     private String lastName;
     private String cpf;
-    
-    public User(String name, String lastName, String cpf){
+
+    public User(String name, String lastName, String cpf) {
         this.name = name;
         this.lastName = lastName;
         this.cpf = cpf;
     }
-    
+
     public User() {
-        
+
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+        
 }
--- src/main/java/br/com/mycompany/mybank/MyBank.java
+++ src/main/java/br/com/mycompany/mybank/MyBank.java
@@ -3,6 +3,9 @@
  */
 package br.com.mycompany.mybank;
 
+import br.com.mycompany.entity.Bank;
+import br.com.mycompany.entity.User;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -14,15 +17,68 @@ public class MyBank {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        
+        Random cc = new Random();
+        
+        Bank bank = new Bank(cc.nextInt(30), 0);
 
         boolean exit = false;
+
+        System.out.println("-----------------*MY BANK*-----------------");
+        System.out.println("Qual seu nome? ");
+        String name = scan.nextLine();
+        System.out.println("Qual seu sobrenome? ");
+        String lastName = scan.nextLine();
+        System.out.println("Qual seu cpf? ");
+        String cpf = scan.nextLine();
+        System.out.println("\n");
+
+        User user = new User(name, lastName, cpf);
 
         do {
             System.out.println("-----------------*MY BANK*-----------------");
             System.out.println("\n");
-            System.out.println("");
-            
+            System.out.println("Conta: " + bank.getCc() + "\nUsuário: " + user.getName() + " " + user.getLastName());
+            System.out.println("Qual operação deseja realizar? \n");
+
+            System.out.println("""
+                               1. Saldo. 
+                               2. Depósito. 
+                               3. Saque. 
+                               4. Sair""");
+            System.out.println("-------------------------------------------");
+
+            try {
+
+                int option = scan.nextInt();
+
+                switch (option) {
+                    case 1 ->
+                        System.out.println("Saldo: R$" + bank.checkBalance());
+                    case 2 -> {
+                        System.out.println("Valor a ser depósitado: ");
+                        float value = scan.nextFloat();
+                        bank.deposit(value);
+                    }
+                    case 3 -> {
+                        System.out.println("Quanto deseja sacar: ");
+                        float withdraw = scan.nextFloat();
+                        bank.withdraw(withdraw);
+                    }
+                    case 4 -> {
+                        System.out.println("Obrigado por utilizar nossos serviços!");
+                        exit = true;
+                    }
+                    default ->
+                        throw new AssertionError();
+                }
+            } catch (AssertionError e) {
+                System.out.println("Opção inválida. Insira o número correto!");
+            }
+
         } while (!exit);
 
+        
+        
     }
 }
