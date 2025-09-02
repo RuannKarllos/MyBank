f54e9f1e477fa36e881a2f70c8bc76154b888f20
RuannKarllos <ruannkarlloss@gmail.com>
2025-09-02 20:20:11 -0300

    update

---
+++ src/main/java/br/com/mycompany/controller/controller.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.mycompany.controller;
+
+/**
+ *
+ * @author Ruann
+ */
+public interface controller {
+    public void checkBalance();
+    public void withdraw(double value);
+    public void deposit(double value);
+}
